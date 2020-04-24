19d0b1cd6c9fc486735648f0ba8567a5a034695b
George Gugulea <gugulea@gmail.com>
2020-04-24 13:02:39 +0300

    refactory: creted modules

---
+++ logger.py
@@ -0,0 +1,21 @@
+import traceback
+
+class Logger:
+    @staticmethod
+    def LogError(msg):
+        print("ERROR: ", msg)
+
+    @staticmethod
+    def LogInfo(msg):
+        print("INFO: ", msg)
+
+    @staticmethod
+    def LogException(msg, ex):
+        print("ERROR: {0}: {1}".format(msg, ex))
+
+    @staticmethod
+    def LogExceptionTrace(msg, ex):
+        print("ERROR: ", msg)
+        print('-'*60)
+        traceback.print_exc(file=sys.stdout)
+        print('-'*60)
---
+++ options.py
@@ -0,0 +1,42 @@
+import getopt
+import pathlib
+
+class Options:
+    def __init__(self):
+        self.urlLotl = 'https://ec.europa.eu/tools/lotl/eu-lotl.xml'
+        self.workingDir = pathlib.Path('/tmp/etsi-plugtests/etl')
+        self.force = False
+        # self.wdPath = pathlib.Path(self.workingDir)
+
+    def localTListPath(self):
+        return self.workingDir / "trustlists"
+
+    def localTrustCertPath(self):
+        return self.workingDir / "trustcerts"
+
+    def localUnTrustCertPath(self):
+        return self.workingDir / "untrustcerts"
+
+    def parseCommandLine(self, argv):
+        try:
+            opts, args = getopt.getopt(
+                argv, "hf", ["workingdir="])
+        except getopt.GetoptError:
+            self.printHelp()
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                self.printHelp()
+                sys.exit()
+            elif opt == '-f':
+                self.force = True
+            elif opt in ("--workingdir"):
+                # self.workingDir = arg
+                self.workingDir = pathlib.Path(arg)
+
+    def printHelp(self):
+        print('''
+            -h              print this help and exit.
+            -f              force download. Will replace the file if it already exists. Default is False.
+            --workingdir    local working directory
+            ''')
